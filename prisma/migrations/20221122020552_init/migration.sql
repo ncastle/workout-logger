8d9c0f3d9c91ff4d919d4665d262f79ed0c08742
Nick Castle <nick@uprighted.com>
2022-11-21 21:07:38 -0500

    add initial prisma mysql schema and migrations

---
+++ prisma/migrations/20221122020552_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Exercise` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `exercise` VARCHAR(191) NOT NULL,
+    `weight` INTEGER NOT NULL,
+    `reps` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Exercise` ADD CONSTRAINT `Exercise_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
