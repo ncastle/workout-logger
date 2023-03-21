4558303cac3489cc1ce19a25cef02d293533dcc1
Nick Castle <castle5896@gmail.com>
2023-03-21 13:03:51 -0400

    Add Day prisma model

---
+++ prisma/migrations/20221222201253_create_day_model_in_schema/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE `Exercise` ADD COLUMN `dayId` VARCHAR(191) NULL;
+
+-- CreateTable
+CREATE TABLE `Day` (
+    `id` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Exercise` ADD CONSTRAINT `Exercise_dayId_fkey` FOREIGN KEY (`dayId`) REFERENCES `Day`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Day` ADD CONSTRAINT `Day_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
